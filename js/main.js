cec21504e22d37977ca535f2d179d6f481b82a3e
Elena Shkurenko <shkurenko.elen@gmail.com>
2022-06-22 10:11:53 +0300

    Внесены правки

--- js/main.js
+++ js/main.js
@@ -1,21 +1,18 @@
 function getRandomIntInclusive(min, max) {
   if(min < 0 || max <= min){
-    console.log('Ошибка!');
+    throw new Error('Ошибка!');
   }else {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-  
 }
 
 getRandomIntInclusive(0, 10);
 
-
-
 function getRandomNumber(min, max, legth){
   if(min < 0 || max <= min){
-    console.log('Ошибка!');
+    throw new Error('Ошибка! 2');
   }else {
     return (Math.random() * (max - min) + min).toFixed(legth);
   }
